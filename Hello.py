16389bf42c44641bd8c37289872f65780927ce69
ChunyangDai <chunyangdai@outlook.com>
2020-10-31 14:22:37 +0800

    cookie

--- Hello.py
+++ Hello.py
@@ -1,6 +1,6 @@
 # coding=UTF-8
 
-from flask import Flask, redirect, url_for, request, render_template
+from flask import Flask, redirect, url_for, request, render_template, make_response
 
 app = Flask(__name__)
 
@@ -77,6 +77,28 @@ def result():
     if request.method == 'POST':
         result = request.form
         return render_template("result.html", result=result)
+
+
+# Cookie
+@app.route("/set_cookies")
+def set_cookie():
+    resp = make_response("success")
+    resp.set_cookie("w3cshool", "w3cshool", max_age=3600)
+    return resp
+
+
+@app.route("/get_cookies")
+def get_cookie():
+    cookie_1 = request.cookies.get("w3cshool")  # 获取名字为Itcast_1对应cookie的值
+    return cookie_1
+
+
+@app.route("/delete_cookies")
+def delete_cookie():
+    resp = make_response("del success")
+    resp.delete_cookie("w3cshool")
+    # 删除，只是让 cookie 过期。
+    return resp
 
 
 if __name__ == '__main__':
